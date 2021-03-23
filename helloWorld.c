e543a895f000fe12d63e16f9748e106e1ef57540
caroollimabr <carool.limabr@gmail.com>
2021-03-23 19:57:34 -0300

    ajuste

--- helloWorld.c
+++ helloWorld.c
@@ -4,7 +4,7 @@
 int main (void)
 {
     printf("Hello, world!\n");
-    printf("This is a program written in C\n")
+    printf("This is a program written in C\n");
 
     system("pause");
     return 0;
