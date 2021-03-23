1c972610c5cc7fa6bfb62d94e5df3e1e2cd67bad
caroollimabr <carool.limabr@gmail.com>
2021-03-23 19:13:48 -0300

    primeiros programas

---
+++ helloWorld.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (void)
+{
+    printf("Hello, world!\n");
+    printf("This is a program written in C\n")
+
+    system("pause");
+    return 0;
+}
